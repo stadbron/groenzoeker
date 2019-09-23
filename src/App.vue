cd3e5610e4d28621e87291a44694b0184e5de70f
Bas Peeters <bas@peete.rs>
2019-09-23 15:13:12 +0200

    Setup vue.js with webpack Closes #1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+      message: "Groenzoeker"
+    })
+  }
+</script>
+
+<style scoped>
+
+</style>
